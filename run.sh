f6a95b2c770fd7ee0fffe3ae933ad46d856598f8
devel-rajkumar <rajkumar.mandal@koinpark.com>
2025-05-06 12:50:06 +0530

    Run file

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install dependencies
+echo "Installing dependencies..."
+pip install kafka-python matplotlib
+
+# Run the Kafka consumer application
+echo "Running kafka_consumer.py..."
+python3 kafka_consumer.py
